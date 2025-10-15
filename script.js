72ac499a9fa0affe6d38b8ea468c8b4e063a55e6
Gray593 <btgray6@gmail.com>
2025-10-15 10:09:26 +0100

    image added

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-async function asyncCall() {
+/**
+ * upload img
+ * draw image onto canvas
+ * split canvas into segments
+ * ensure segments are draggable
+ * correct pieces snap together 
+ */
 
-}
-  
-asyncCall();
+const canvas = document.getElementById("puzzle")
+const ctx = canvas.getContext("2d")
+const userImage = document.getElementById("puzzleImage")
+ctx.drawImage(userImage,10,10)
