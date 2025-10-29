1cc063f24dd80eb8b27bc810238d3f8524c839cb
Gray <btgray6@gmail.com>
2025-10-29 15:18:30 +0000

    more puzzle images added and image randomiser added to js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-/**
- * upload img
- * draw image onto canvas
- * split canvas into segments
- * ensure segments are draggable
- * correct pieces snap together 
- */
-
+const images = [
+    "./PuzzleImages/image2.png",
+    "./PuzzleImages/image3.png",
+    "./PuzzleImages/image4.png",
+    "./PuzzleImages/image5.png",
+    "./PuzzleImages/image6.png",
+    "./PuzzleImages/image7.png",
+    "./PuzzleImages/image8.png",
+    "./PuzzleImages/image9.png"
+  ];
+console.log(images)
 const canvas = document.getElementById("puzzle")
 const ctx = canvas.getContext("2d")
-const userImage = document.getElementById("puzzleImage")
-ctx.drawImage(userImage,0,0,750,750)
+const userImage = new Image()
+userImage.src = images[Math.floor(Math.random() * images.length)]
+userImage.onload = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(userImage, 0, 0, 750, 750)
+}
