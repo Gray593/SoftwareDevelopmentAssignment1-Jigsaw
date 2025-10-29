5de2105b3d84ac1755e1cf519c4cb87002be9b54
Gray <btgray6@gmail.com>
2025-10-29 16:44:36 +0000

    buttons added

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const images = [
     "./PuzzleImages/image8.png",
     "./PuzzleImages/image9.png"
   ];
-console.log(images)
-const canvas = document.getElementById("puzzle")
+// const containing the urls of all the images used in the puzzle
+
+const canvas = document.getElementById("puzzle") // assigns the html canvas as the canvas variable
 const ctx = canvas.getContext("2d")
-const userImage = new Image()
-userImage.src = images[Math.floor(Math.random() * images.length)]
-userImage.onload = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(userImage, 0, 0, 750, 750)
+const userImage = new Image() //creates <img> tag
+userImage.src = images[Math.floor(Math.random() * images.length)] //picks a random url from the images constant
+userImage.onload = () => { //function executes when the above img has been loaded
+    ctx.clearRect(0, 0, canvas.width, canvas.height)// sets up the canvas by clearing it
+    ctx.drawImage(userImage, 0, 0, 750, 750) //draws the image 
 }
+
